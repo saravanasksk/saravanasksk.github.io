017896abad3e7132165eeb0352d35c9a4d91d38d
SARAVANAKUMAR S K <saravanasksk@gmail.com>
2018-11-29 19:40:12 +0530

    Update Contacts.php

--- Contacts.php
+++ Contacts.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_POST["submit"]) {
-    $recipient="your@email.address";
+    $recipient="saravanasksk@gmail.com";
     $subject="Form to email message";
     $sender=$_POST["sender"];
     $senderEmail=$_POST["senderEmail"];
